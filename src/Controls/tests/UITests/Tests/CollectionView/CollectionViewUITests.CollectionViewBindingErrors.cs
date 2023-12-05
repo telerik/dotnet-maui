3e04a5a19e478481b5fd33d90129deb3a8a37293
Javier Suárez <javiersuarezruiz@hotmail.com>
2023-12-05 11:56:49 +0100

    Adding more tests

---
+++ src/Controls/tests/UITests/Tests/CollectionView/CollectionViewUITests.CollectionViewBindingErrors.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests
+{
+	public class CollectionViewBindingErrorsUITests : CollectionViewUITests
+	{
+		public CollectionViewBindingErrorsUITests(TestDevice device)
+			: base(device)
+		{
+		}
+
+		[Test]
+		public void NoBindingErrors()
+		{
+			App.ScrollTo("CollectionViewBindingErrors");
+			App.Click("CollectionViewBindingErrors");
+
+			App.WaitForElement("WaitForStubControl");
+			App.WaitForNoElement("Binding Errors: 0");
+		}
+	}
+}
