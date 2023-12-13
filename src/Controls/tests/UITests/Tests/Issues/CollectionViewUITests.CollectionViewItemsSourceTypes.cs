000006f7466dc6ae2b70957b70bbfb0ee90a513e
Javier Suárez Ruiz <jsuarez@microsoft.com>
2023-12-13 11:58:13 +0100

    More tests

---
+++ src/Controls/tests/UITests/Tests/Issues/CollectionViewUITests.CollectionViewItemsSourceTypes.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using UITest.Appium;
+using UITest.Core;
+
+namespace Microsoft.Maui.AppiumTests
+{
+	public class CollectionViewItemsSourceTypesUITests : _IssuesUITest
+	{
+		public CollectionViewItemsSourceTypesUITests(TestDevice device)
+			: base(device)
+		{
+		}
+
+		public override string Issue => "CollectionView ItemsSource Types";
+
+		// CollectionViewItemsSourceTypesDisplayAndDontCrash (src\Compatibility\ControlGallery\src\Issues.Shared\CollectionViewItemsSourceTypes.cs)
+		[Test]
+		public void CollectionViewItemsSourceTypesDisplayAndDontCrash()
+		{
+			App.WaitForNoElement("900");
+		}
+	}
+}
