d95746bbef98f4fed7cfa2b6ba571657d11c1c24
epopova <Elena.Popova@progress.com>
2024-11-25 17:05:11 +0200

    Rebase on latest main

--- src/Core/tests/DeviceTests.Shared/MauiProgramDefaults.cs
+++ src/Core/tests/DeviceTests.Shared/MauiProgramDefaults.cs
@@ -74,8 +74,8 @@ namespace Microsoft.Maui.DeviceTests
 
 			appBuilder.ConfigureMauiHandlers(handlers =>
 				{
-					handlers.AddHandler<Microsoft.Maui.Controls.CollectionView, Microsoft.Maui.Controls.Handlers.Items2.CollectionViewHandler2>();
-					handlers.AddHandler<Microsoft.Maui.Controls.CarouselView, Microsoft.Maui.Controls.Handlers.Items2.CarouselViewHandler2>();
+					handlers.AddHandler<Microsoft.Maui.Controls.CollectionView, Microsoft.Maui.Controls.Handlers.Items.CollectionViewHandler>();
+					handlers.AddHandler<Microsoft.Maui.Controls.CarouselView, Microsoft.Maui.Controls.Handlers.Items.CarouselViewHandler>();
 				});
 
 #endif
